38163f765e7b7b8c2d2f32972cd199f9d100a73b
ChrisCh <>
2022-07-14 22:02:00 +0300

    Add RemoveSnipe

---
+++ src/main/kotlin/com/chrisch/discordbot/command/RemoveSnipe.kt
@@ -0,0 +1,46 @@
+package com.chrisch.discordbot.command
+
+import discord4j.core.event.domain.interaction.MessageInteractionEvent
+import discord4j.discordjson.json.ApplicationCommandRequest
+import discord4j.rest.util.Permission
+import kotlinx.coroutines.reactor.awaitSingle
+import kotlinx.coroutines.reactor.awaitSingleOrNull
+import kotlinx.coroutines.reactor.mono
+import org.springframework.stereotype.Service
+import reactor.core.publisher.Mono
+
+@Service
+class RemoveSnipe : CommandHandler<MessageInteractionEvent> {
+
+    override val name: String = "Remove snipe"
+
+    override val command: ApplicationCommandRequest
+        get() = ApplicationCommandRequest.builder()
+            .name(name)
+            .type(3)
+            .build()
+
+    override fun handle(event: MessageInteractionEvent): Mono<Void> {
+        if (event.interaction.member.isEmpty) return Mono.empty()
+
+        val member = event.interaction.member.orElseThrow()
+        val message = event.resolvedMessage
+
+        if (message.embeds.size == 1 && message.embeds[0].title.orElse("title").equals("Snipe")) {
+            return mono {
+                if (!member.basePermissions.awaitSingle().contains(Permission.ADMINISTRATOR)) {
+                    event.reply("You don't have permission to use this command.").withEphemeral(true)
+                        .awaitSingleOrNull()
+                    return@mono
+                }
+
+                event.deferReply().awaitSingleOrNull()
+                message.embeds.removeLast()
+                message.delete().awaitSingleOrNull()
+                event.deleteReply().awaitSingleOrNull()
+            }.then()
+        }
+
+        return event.reply("That is not a snipe.").withEphemeral(true)
+    }
+}
