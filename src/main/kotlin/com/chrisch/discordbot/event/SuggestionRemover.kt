f17a74d28b0ec556aac0bbe575cffefd901a2e54
ChrisCh <>
2022-07-15 00:53:47 +0300

    Add SuggestionRemover

---
+++ src/main/kotlin/com/chrisch/discordbot/event/SuggestionRemover.kt
@@ -0,0 +1,73 @@
+package com.chrisch.discordbot.event
+
+import discord4j.common.util.Snowflake
+import discord4j.core.event.domain.message.ReactionAddEvent
+import discord4j.core.`object`.entity.channel.TopLevelGuildMessageChannel
+import discord4j.core.spec.EmbedCreateSpec
+import discord4j.rest.util.Color
+import kotlinx.coroutines.reactor.awaitSingle
+import kotlinx.coroutines.reactor.awaitSingleOrNull
+import kotlinx.coroutines.reactor.mono
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+import reactor.core.publisher.Mono
+
+@Service
+class SuggestionRemover : EventListener<ReactionAddEvent> {
+
+    @Value("\${SUGGESTIONS_CHANNEL_ID}")
+    private val suggestionsChannelId: String = ""
+
+    @Value("\${LOGS_CHANNEL_ID}")
+    private val logsChannelId: String = ""
+
+    override val eventType: Class<ReactionAddEvent> = ReactionAddEvent::class.java
+
+    override fun execute(event: ReactionAddEvent): Mono<Void> {
+        if (event.member.isEmpty) return Mono.empty()
+
+        val member = event.member.orElseThrow()
+
+        if (member.isBot || event.channelId != Snowflake.of(suggestionsChannelId)) {
+            return Mono.empty()
+        }
+
+        return mono {
+            val message = event.message.awaitSingle()
+            val reactions = message.reactions
+
+            val upvoteReactions = reactions.filter { it.emoji.asEmojiData().name().orElseThrow() == "upvote" }
+            val downvoteReactions = reactions.filter { it.emoji.asEmojiData().name().orElseThrow() == "downvote" }
+
+            val nrUpvotes = upvoteReactions.firstOrNull()?.count ?: 0
+            val nrDownvotes = downvoteReactions.firstOrNull()?.count ?: 0
+
+            val diff = nrDownvotes - nrUpvotes
+
+            if (diff >= voteDiffLimit) {
+                message.delete().awaitSingleOrNull()
+
+                val embed = EmbedCreateSpec.builder()
+                    .color(Color.RED)
+                    .title("Suggestion")
+                    .addField("Author", message.author.orElseThrow().tag, false)
+                    .addField("Message", message.content, false)
+                    .build()
+
+                message.guild
+                    .flatMap { it.getChannelById(Snowflake.of(logsChannelId)) }
+                    .ofType(TopLevelGuildMessageChannel::class.java)
+                    .flatMap {
+                        it.createMessage(
+                            "Deleted suggestion because downvotes ($nrDownvotes)" +
+                                    " - upvotes ($nrUpvotes) >= $voteDiffLimit"
+                        ).withEmbeds(embed)
+                    }.awaitSingle()
+            }
+        }.then()
+    }
+
+    companion object {
+        private const val voteDiffLimit = 5
+    }
+}
