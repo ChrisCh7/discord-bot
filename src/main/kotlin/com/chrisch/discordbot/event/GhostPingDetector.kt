d3016b7ec8fa7ee7c02dd62054e0b9fc743dd55e
ChrisCh <>
2022-07-14 22:44:11 +0300

    Add GhostPingDetector

---
+++ src/main/kotlin/com/chrisch/discordbot/event/GhostPingDetector.kt
@@ -0,0 +1,66 @@
+package com.chrisch.discordbot.event
+
+import com.chrisch.discordbot.util.Utils.getMessageUrl
+import discord4j.core.event.domain.message.MessageDeleteEvent
+import discord4j.core.`object`.entity.Message
+import discord4j.core.spec.EmbedCreateSpec
+import discord4j.rest.util.Color
+import org.springframework.stereotype.Service
+import reactor.core.publisher.Mono
+
+@Service
+class GhostPingDetector : EventListener<MessageDeleteEvent> {
+    override val eventType: Class<MessageDeleteEvent> = MessageDeleteEvent::class.java
+
+    override fun execute(event: MessageDeleteEvent): Mono<Void> {
+        if (event.message.isEmpty) return Mono.empty()
+
+        val message = event.message.orElseThrow()
+
+        if (message.author.map { it.isBot }.orElse(true)) {
+            return Mono.empty()
+        }
+
+        if (message.userMentionIds.isEmpty() && message.roleMentionIds.isEmpty()) {
+            return Mono.empty()
+        }
+
+        if (message.userMentions.stream().allMatch { user ->
+                user.id == message.author.orElseThrow().id || user.isBot
+            }
+        ) {
+            return Mono.empty()
+        }
+
+        var repliedToMessage: Message? = null
+        var repliedToUserTag: String? = null
+
+        if (message.referencedMessage.isPresent) {
+            repliedToMessage = message.referencedMessage.orElseThrow()
+            repliedToUserTag = repliedToMessage.author.map { it.tag }.orElse(null)
+        }
+
+        val embed = EmbedCreateSpec.builder()
+            .color(Color.GREEN)
+            .title("Ghost Ping Detected!")
+            .apply {
+                if (repliedToUserTag != null) {
+                    addField("Author", message.author.orElseThrow().tag, true)
+                    addField("Reply to", repliedToUserTag, true)
+                } else {
+                    addField("Author", message.author.orElseThrow().tag, false)
+                }
+            }
+            .addField("Message", message.content, false)
+            .apply {
+                if (repliedToMessage != null) {
+                    addField("Message replied to", getMessageUrl(repliedToMessage), true)
+                }
+            }
+            .build()
+
+        return event.channel
+            .flatMap { it.createMessage(embed) }
+            .then()
+    }
+}
